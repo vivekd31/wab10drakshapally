77151b3b2daedaacd8fba30287978862cb83be45
vivekd31 <77740150+vivekd31@users.noreply.github.com>
2021-03-31 15:41:38 -0500

    added stars pug and js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bakeryRouter = require('./routes/bakery');
+var starsRouter = require('./routes/stars');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bakery', bakeryRouter);
+app.use('/stars', starsRouter);
 
 
 
